97f335afdd469f7be750dd9eefe35f7176c1a44e
Samantha Yeargin <slyeargin@gmail.com>
2014-12-10 20:45:26 -0600

    Removed /app

--- app/routes/home.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.index = (req, res)=>{
-  res.render('home/index', {title: 'iTNerary'});
-};
-
-exports.help = (req, res)=>{
-  res.render('home/help', {title: 'Node.js: Help'});
-};
